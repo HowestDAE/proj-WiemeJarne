aaeaa0f72577fa134a3a3113f045f057a0ffa564
Jarne Wieme <jarne.wieme@telenet.be>
2023-03-21 19:53:58 +0100

    added model classes Game, Store and Deal

---
+++ Project/Model/Deal.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model
+{
+    public class Deal
+    {
+        [JsonProperty(PropertyName = "dealID")]
+        public string dealId { get; set; }
+
+        [JsonProperty(PropertyName = "storeID")]
+        public string StoreId { get; set; }
+
+        [JsonProperty(PropertyName = "gameID")]
+        public string gameId { get; set; }
+
+        [JsonProperty(PropertyName = "salePrice")]
+        public float SalePrice { get; set; }
+
+        [JsonProperty(PropertyName = "normalPrice")]
+        public float NormalPrice { get; set; }
+
+        [JsonProperty(PropertyName = "isOnSale")]
+        public bool OnSale { get; set; }
+
+        [JsonProperty(PropertyName = "steamRatingText")]
+        public string SteamRatingText { get; set; }
+
+        [JsonProperty(PropertyName = "steamRatingPercent")]
+        public string SteamRatingPercent { get; set; }
+
+        [JsonProperty(PropertyName = "steamRatingCount")]
+        public string SteamReviewCount { get; set; }
+
+        [JsonProperty(PropertyName = "releaseDate")]
+        public int StartDate { get; set; } //in unix timestamp format
+
+        [JsonProperty(PropertyName = "lastChange")]
+        public int EndDate { get; set; } //in unix timestamp format
+
+        [JsonProperty(PropertyName = "dealRating")]
+        public float Rating { get; set; }
+
+        [JsonProperty(PropertyName = "thumb")]
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Project/Model/Store.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model
+{
+    public class Store
+    {
+        [JsonProperty(PropertyName = "storeID")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "steam")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "isActive")]
+        public bool IsActive { get; set; }
+
+        [JsonProperty(PropertyName = "banner")]
+        public string BannerUrl { get; set; }
+
+        [JsonProperty(PropertyName = "logo")]
+        public string LogoUrl { get; set; }
+
+        [JsonProperty(PropertyName = "icon")]
+        public string IconUrl { get; set; }
+    }
+}
